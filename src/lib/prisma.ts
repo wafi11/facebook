cccf87f66300b3be0ad4d3e2748db3dd68a2c211
unknown <wafiwafiwafi90@gmail.com>
2024-07-27 16:04:38 -0700

    p

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-export default prisma;
+const db = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export default db;
